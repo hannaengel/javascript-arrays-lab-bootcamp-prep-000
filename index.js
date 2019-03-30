6763560770c8200188b88ea8ba773268b56cb674
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-30 06:37:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
   function destructivelyAppendKitten(name){
@@ -20,16 +21,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 }
 
   function appendKitten(name){
-
   return [...kittens, name]
 }
   function prependKitten(name){
   return [name, ...kittens]
-
-  function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1);
-  }
-
-  function removeFirstKitten() {
-  return kittens.slice(1);
-  }
