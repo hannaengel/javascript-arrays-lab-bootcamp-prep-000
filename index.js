f856db4f29c28533a57bb154cb666dfdb665d102
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-30 06:32:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-function destructivelyAppendKitten(name){
+  function destructivelyAppendKitten(name){
   kittens.push(name)
-return kittens}
+  return kittens}
 
-function destructivelyPrependKitten(name){
+  function destructivelyPrependKitten(name){
   kittens.unshift(name)
   return kittens
 }
 
-function destructivelyRemoveLastKitten(){
+  function destructivelyRemoveLastKitten(){
   kittens.pop()
   return kittens
 }
 
-function destructivelyRemoveFirstKitten(){
+  function destructivelyRemoveFirstKitten(){
   kittens.shift()
   return kittens
 }
 
-function appendKitten(name){
+  function appendKitten(name){
 
   return [...kittens, name]
 }
-function prependKitten(name){
+  function prependKitten(name){
   return [name, ...kittens]
 
-function removeLastKitten() {
+  function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
   }
 
-function removeFirstKitten() {
+  function removeFirstKitten() {
   return kittens.slice(1);
   }
